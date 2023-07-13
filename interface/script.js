2576c917dbf1bd58e0237a45e1b18e84ed2dd0e0
Teolin <teolin@gmail.com>
2023-07-13 16:56:32 +0300

    more readable scripts file

--- interface/script.js
+++ interface/script.js
@@ -1,50 +1,68 @@
-// Get the console element
-const consoleElement = document.getElementById('console');
-
-// Override the default console log function to print to consoleElement
+// ------- FUNCTION DEFINITIONS ---------
 function log(message) {
-  const p = document.createElement('p');
-  p.textContent = message;
-  // p.style.margin = '0';
-  // p.style.padding = '0';
-  consoleElement.appendChild(p);
+  const consoleElement = document.getElementById('console')
+  const p = document.createElement('p')
+  p.textContent = message
+  consoleElement.appendChild(p)
 }
-// Redirect console output to the log
-console.log = log;
 
-const mods = document.querySelectorAll('.mod');
-const audio = new Audio('hover.mp3');
+function getJsonData(response) {
+  return response.json()
+}
 
-// Load the checkboxes from the JSON file
-fetch('mods.json')
-  .then(response => response.json())
-  .then(data => {
-    const checkboxesDiv = document.getElementById('checkboxes');
-    data.forEach(checkbox => {
-      // Create the checkbox element and set its attributes
-      const checkboxElement = document.createElement('div');
-      checkboxElement.classList.add('mod');
-      checkboxElement.innerHTML = `
-          <input type="checkbox" id="${checkbox.id}">
-          <label for="${checkbox.id}">${checkbox.description}</label>
-        `;
+function createCheckboxElement(checkbox) {
+  const checkboxElement = document.createElement('div')
+  checkboxElement.classList.add('mod')
+  checkboxElement.innerHTML = `
+    <input type="checkbox" id="${checkbox.id}">
+    <label for="${checkbox.id}">${checkbox.description}</label>
+  `
+  return checkboxElement
+}
 
-      checkboxesDiv.appendChild(checkboxElement);
-    });
+function handleModMouseEnter(mod) {
+  console.log('hovering...')
+  const audio = new Audio('hover.mp3')
+  audio.currentTime = 0
+  audio.play()
+  const output = document.querySelector('#output')
+  output.textContent = mod.textContent
+  mod.hoverTimeout = setTimeout(() => { mod.dispatchEvent(mod.longHoverEvent) }, 1000)
+}
 
-    // Return the .mod elements
-    return checkboxesDiv.querySelectorAll('.mod');
+function handleModMouseLeave(mod) {
+  clearTimeout(mod.hoverTimeout)
+}
+
+function handleLongHover(mod) {
+  console.log('Hello')
+}
+
+function attachEvents() {
+  const mods = document.querySelectorAll('.mod')
+  mods.forEach(mod => {
+    mod.hoverTimeout = null
+    mod.longHoverEvent = new Event('longhover')
+    mod.addEventListener('mouseenter', () => handleModMouseEnter(mod))
+    mod.addEventListener('mouseleave', () => handleModMouseLeave(mod))
+    mod.addEventListener('longhover', () => handleLongHover(mod))
   })
-  .then(mods => {
-    const audio = new Audio('hover.mp3');
+}
 
-    mods.forEach(mod => {
-      mod.addEventListener('mouseenter', () => {
-        console.log('hovering');
-        audio.currentTime = 0;
-        audio.play();
-        const output = document.querySelector('#output');
-        output.textContent = mod.textContent;
-      });
-    });
-  });
+function generateModsList(data) {
+  const checkboxesDiv = document.getElementById('checkboxes')
+  data.forEach(checkbox => {
+    const checkboxElement = createCheckboxElement(checkbox)
+    checkboxesDiv.appendChild(checkboxElement)
+  })
+  return checkboxesDiv.querySelectorAll('.mod')
+}
+
+// ----------- EXECUTE ------------
+// Redirect console output to the UI
+console.log = log
+// Generate mods list and attach events
+fetch('mods.json')
+  .then(getJsonData)
+  .then(generateModsList)
+  .then(attachEvents)
--- modules/do-test.ps1
+++ modules/do-test.ps1
@@ -1,15 +1,60 @@
-$dir = Split-Path -Parent $MyInvocation.MyCommand.Path
-$folderPath = "$dir\test"
-$fileBaseName = "test"
-$fileExtension = ".txt"
-$fileNumber = 1
+function Check-Test {
+    $dir = Split-Path -Parent $MyInvocation.MyCommand.Path
+    $folderPath = "$dir\test"
+    $fileExtension = ".txt"
 
-if (-not (Test-Path $folderPath)) {
-    New-Item -ItemType Directory -Path $folderPath
+    if (Test-Path $folderPath) {
+        $files = Get-ChildItem -Path $folderPath -Filter "*$fileExtension" -File
+        if ($files.Count -gt 0) {
+            Write-Host "Text files in ${folderPath}:"
+            foreach ($file in $files) {
+                Write-Host $file.Name
+            }
+        }
+        else {
+            Write-Host "No text files found in ${folderPath}"
+        }
+    }
+    else {
+        Write-Host "Directory ${folderPath} does not exist"
+    }
 }
 
-while (Test-Path "$folderPath\$fileBaseName$fileNumber$fileExtension") {
-    $fileNumber++
+function Enable-Test {
+    $dir = Split-Path -Parent $MyInvocation.MyCommand.Path
+    $folderPath = "$dir\test"
+    $fileBaseName = "test"
+    $fileExtension = ".txt"
+    $fileNumber = 1
+
+    if (-not (Test-Path $folderPath)) {
+        New-Item -ItemType Directory -Path $folderPath
+    }
+
+    while (Test-Path "$folderPath\$fileBaseName$fileNumber$fileExtension") {
+        $fileNumber++
+    }
+
+    New-Item -ItemType File -Path "$folderPath\$fileBaseName$fileNumber$fileExtension"
 }
 
-New-Item -ItemType File -Path "$folderPath\$fileBaseName$fileNumber$fileExtension"
+function Disable-Test {
+    $dir = Split-Path -Parent $MyInvocation.MyCommand.Path
+    $folderPath = "$dir\test"
+    $fileBaseName = "test"
+    $fileExtension = ".txt"
+    $fileNumber = 1
+
+    while (Test-Path "$folderPath\$fileBaseName$fileNumber$fileExtension") {
+        $fileNumber++
+    }
+
+    if ($fileNumber -gt 1) {
+        $fileNumber--
+        Remove-Item -Path "$folderPath\$fileBaseName$fileNumber$fileExtension"
+        Write-Host "Deleted $fileBaseName$fileNumber$fileExtension in $folderPath"
+    }
+    else {
+        Write-Host "No files found to delete in $folderPath"
+    }
+}
