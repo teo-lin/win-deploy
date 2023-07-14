0a0ecb414e05c303aa0a6742274adc295d410d8b
Teolin <teolin@gmail.com>
2023-07-14 16:07:48 +0300

    automatically generated tabs and buttons

--- interface/script.js
+++ interface/script.js
@@ -1,3 +1,13 @@
+// Hijack the console output and redirect it to the UI
+function log(message) {
+  const consoleElement = document.getElementById('console')
+  const p = document.createElement('p')
+  p.textContent = message
+  consoleElement.appendChild(p)
+}
+console.log = log
+
+// Generate panel1 automatically from json
 function openTab(evt, tabName) {
   var i, tabcontent, tablinks
 
@@ -14,63 +24,75 @@ function openTab(evt, tabName) {
   document.getElementById(tabName).style.display = "block"
   evt.currentTarget.className += " active"
 }
-function log(message) {
-  const consoleElement = document.getElementById('console')
-  const p = document.createElement('p')
-  p.textContent = message
-  consoleElement.appendChild(p)
-}
-function getJsonData(response) {
-  return response.json()
-}
 function generateModsList(data) {
   function createCheckboxElement(checkbox) {
-    const checkboxElement = document.createElement('div')
-    checkboxElement.classList.add('mod')
+    const checkboxElement = document.createElement("div")
+    checkboxElement.classList.add("mod")
     checkboxElement.innerHTML = `
-    <input type="checkbox" id="${checkbox.file}">
-    <label for="${checkbox.id}">${checkbox.info}</label>
-  `
+      <input type="checkbox" id="${checkbox.file}">
+      <label for="${checkbox.id}">${checkbox.info}</label>
+    `
     return checkboxElement
   }
-  const checkboxesDiv = document.getElementById('checkboxes')
-  data.forEach(checkbox => {
+
+  const checkboxesDiv = document.createElement("div")
+  data.forEach((checkbox) => {
     const checkboxElement = createCheckboxElement(checkbox)
     checkboxesDiv.appendChild(checkboxElement)
   })
-  return checkboxesDiv.querySelectorAll('.mod')
+  return checkboxesDiv.querySelectorAll(".mod")
+}
+function generateHtml(data) {
+  const tabContainer = document.getElementById("tab-container")
+  const tabContentContainer = document.getElementById("tab-content-container")
+
+  Object.keys(data).forEach((tabName, index) => {
+    const tabButton = document.createElement("button")
+    tabButton.classList.add("tablinks")
+    tabButton.textContent = tabName
+    tabButton.onclick = (event) => openTab(event, `tab-${index}`)
+
+    const tabContent = document.createElement("div")
+    tabContent.classList.add("tabcontent")
+    tabContent.id = `tab-${index}`
+
+    const mods = generateModsList(data[tabName])
+    mods.forEach((mod) => tabContent.appendChild(mod))
+
+    tabContainer.appendChild(tabButton)
+    tabContentContainer.appendChild(tabContent)
+  })
 }
 function attachEvents() {
   function handleModMouseEnter(mod) {
-    console.log('hovering...')
-    const audio = new Audio('hover.mp3')
+    console.log("hovering...")
+    const audio = new Audio("hover.mp3")
     audio.currentTime = 0
     audio.play()
-    const output = document.querySelector('#output')
+    const output = document.querySelector("#output")
     output.textContent = mod.textContent
-    mod.hoverTimeout = setTimeout(() => { mod.dispatchEvent(mod.longHoverEvent) }, 1000)
+    mod.hoverTimeout = setTimeout(() => {
+      mod.dispatchEvent(mod.longHoverEvent)
+    }, 1000)
   }
   function handleModMouseLeave(mod) {
     clearTimeout(mod.hoverTimeout)
   }
   function handleLongHover(mod) {
-    console.log('Hello')
+    console.log("Hello")
   }
-  const mods = document.querySelectorAll('.mod')
-  mods.forEach(mod => {
+  const mods = document.querySelectorAll(".mod")
+  mods.forEach((mod) => {
     mod.hoverTimeout = null
-    mod.longHoverEvent = new Event('longhover')
-    mod.addEventListener('mouseenter', () => handleModMouseEnter(mod))
-    mod.addEventListener('mouseleave', () => handleModMouseLeave(mod))
-    mod.addEventListener('longhover', () => handleLongHover(mod))
+    mod.longHoverEvent = new Event("longhover")
+    mod.addEventListener("mouseenter", () => handleModMouseEnter(mod))
+    mod.addEventListener("mouseleave", () => handleModMouseLeave(mod))
+    mod.addEventListener("longhover", () => handleLongHover(mod))
   })
 }
 
-// ----------- EXECUTE ------------
-// Redirect console output to the UI
-console.log = log
-// Generate mods list and attach events
-fetch('mods.json')
-  .then(getJsonData)
-  .then(generateModsList)
-  .then(attachEvents)
+fetch("modules.json")
+  .then(jsonFile => jsonFile.json())
+  .then(jsonData => generateHtml(jsonData))
+  .then(() => attachEvents())
+  .catch(error => console.log(error))
